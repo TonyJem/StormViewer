2eb9e1570f2df063d8bacbf5a4c02c20810b38ee
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-17 21:10:14 +0300

    Create Classes for ViewControllers and update UI

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Office-iMac on 2020-09-17.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ StormViewer/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  StormViewer
+//
+//  Created by Office-iMac on 2020-09-17.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
