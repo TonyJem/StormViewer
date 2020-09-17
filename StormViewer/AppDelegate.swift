b91b74852dc90f943082f1b3a83fb0489e8be72f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-17 20:25:13 +0300

    Initial Commit

---
+++ StormViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StormViewer
+//
+//  Created by Office-iMac on 2020-09-17.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Office-iMac on 2020-09-17.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
